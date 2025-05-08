be22654d2522e187b9b0c67123abb8b1c3998bb1
Malik Laet <isklikma@gmail.com>
2025-05-08 15:27:26 -0300

    add react router dom

--- src/app.tsx
+++ src/app.tsx
@@ -3,16 +3,20 @@ import ChoicePage from "./pages/choicepage";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/themes/global";
 
+import { BrowserRouter} from "react-router-dom"
+import { Router } from "./router";
+
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
       <GlobalStyle/>
       <>
-      
       <ChoicePage/>
-      
       </>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
--- src/pages/choicepage/index.tsx
+++ src/pages/choicepage/index.tsx
@@ -1,12 +1,19 @@
 import { Button, Container, Title } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 export default function Choice() {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Title>Escolha uma opção</Title>
-      <Button>Sou empresa</Button>
+      <Button onClick={() => navigate("/company")}>
+          Sou empresa
+        </Button>
 
-      <Button>Sou estudante</Button>
+      <Button onClick={() => navigate("/student")}>
+        Sou estudante
+      </Button>
     </Container>
   );
 }
